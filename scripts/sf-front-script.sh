39f2ccc9f9ec9214670a286d79b414e5b4c0b063
viniciussoares18 <vinicius.souza@sptech.school>
2023-10-29 18:36:14 -0300

    fix: adicionando hierarquia para divisão de front e back

--- scripts/sf-front-script.sh
+++ scripts/sf-front-script.sh
@@ -3,6 +3,6 @@ sudo apt-get update &&
 # Instala o Docker Compose e o Git
 sudo apt-get install docker-compose -y && sudo apt-get install git &&
 # Clona o repositório que contém os recursos de infraestrutura da Safe Food e vai para o diretório do Docker Compose
-sudo git clone https://github.com/Pesquisa-e-Inovacao-Grupo-06/safe-food-infra-resources.git && cd safe-food-infra-resources/docker-compose/for_back &&
+sudo git clone https://github.com/Pesquisa-e-Inovacao-Grupo-06/safe-food-infra-resources.git && cd safe-food-infra-resources/docker-compose/for_front &&
 # Realiza a subida do Docker Compose
 sudo docker-compose up

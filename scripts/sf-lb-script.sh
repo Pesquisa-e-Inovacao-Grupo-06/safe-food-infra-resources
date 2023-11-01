823c20acb29b6ef27d112aa94fb0ade8f5b2b923
viniciussoares18 <vinicius.souza@sptech.school>
2023-10-31 21:08:40 -0300

    fix: ajustes nginx e script

--- scripts/sf-lb-script.sh
+++ scripts/sf-lb-script.sh
@@ -4,10 +4,14 @@ sudo apt-get update &&
 sudo apt-get install docker-compose -y && sudo apt-get install git &&
 # Clona o repositório que contém os recursos de infraestrutura da Safe Food e vai para o diretório do Docker Compose
 sudo git clone https://github.com/Pesquisa-e-Inovacao-Grupo-06/safe-food-infra-resources.git && cd safe-food-infra-resources/docker-compose/for_lb &&
-# Altera o arquivo hosts do servidor
-sudo echo "
-sf-front1.com.br 34.205.109.148
-sf-front2.com.br 54.164.215.72
-" > "/etc/hosts" &&
-# Realiza a subida do Docker Compose
+# # Alterando para usuário root
+# sudo su &&
+# # Altera o arquivo hosts do servidor
+# sudo echo "
+# sf-front1.com.br 34.205.109.148
+# sf-front2.com.br 54.164.215.72
+# " > "/etc/hosts" &&
+# # Alterando para usuário ubuntu
+# sudo su ubuntu &&
+# # Realiza a subida do Docker Compose
 sudo docker-compose up
